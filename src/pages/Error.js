13b019678cb7e55e760f860d572a4fba6becd308
PatrickM-NAVDNA <PatrickM@avdna.com>
2022-03-29 18:39:04 -0500

    Error page created

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import '../styles/error.css'
+import { Link } from 'react-router-dom';
 
 function Error() {
     return (
-        <div>
-            Error, Page not Found
+        <div className='flex'>
+            <img src='https://previews.123rf.com/images/kaymosk/kaymosk1804/kaymosk180400006/100130939-fehler-404-seite-nicht-gefunden-fehler-mit-glitch-effekt-auf-dem-bildschirm-vektorillustration-f%C3%BCr-i.jpg' alt='404 text' /><Link to="/Home"><button>Return Home</button></Link>
         </div>
     );
 }
